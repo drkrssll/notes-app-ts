7430fe72208cd89374827fe8e38e171a798ace87
drkrssll <russellderek01@gmail.com>
2024-08-27 16:22:34 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import NewNote from './Components/NewNote'
+import useLocalStorage from './useLocalStorage'
+import { useMemo } from 'react'
+import { v4 as uuidV4 } from 'uuid'
+import NoteList from './Components/NoteList'
+
+export type Note = {
+  id: string,
+} & NoteData
+
+export type RawNote = {
+  id: string,
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string,
+  markdown: string,
+  tagIds: string[],
+}
+
+export type NoteData = {
+  title: string,
+  markdown: string,
+  tags: Tag[],
+}
+
+export type Tag = {
+  id: string,
+  label: string,
+}
+
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tag: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+    })
+  }
+
+  const addTag = (tag: Tag) => {
+    setTags(prev => [...prev, tag])
+  }
+
+  return (
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<NoteList availableTags={tags} />} />
+        <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
